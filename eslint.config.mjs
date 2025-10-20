e93ba5d5186375c8b2f97a0f45af60c853edfce1
AbdulMalek-swe <abdulmalek.swe.585@gmail.com>
2025-10-20 17:45:45 +0600

    husky stup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,6 @@
-// eslint.config.mjs
-
+import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,19 +18,45 @@ const eslintConfig = [
       'out/**',
       'build/**',
       'next-env.d.ts'
-    ],
-    plugins: {
-      'unused-imports': require('eslint-plugin-unused-imports')
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'off', // turn off default
-      'unused-imports/no-unused-imports': 'error', // remove unused imports
-      'unused-imports/no-unused-vars': [
-        'warn',
-        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
-      ]
-    }
+    ]
   }
 ];
 
 export default eslintConfig;
+
+
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import { FlatCompat } from '@eslint/eslintrc';
+// import unusedImports from 'eslint-plugin-unused-imports';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({ baseDirectory: __dirname });
+
+// const eslintConfig = [
+//   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+//   {
+//     ignores: [
+//       'node_modules/**',
+//       '.next/**',
+//       'out/**',
+//       'build/**',
+//       'next-env.d.ts'
+//     ],
+//     plugins: {
+//       'unused-imports': unusedImports
+//     },
+//     rules: {
+//       '@typescript-eslint/no-unused-vars': 'off',
+//       'unused-imports/no-unused-imports': 'error',
+//       'unused-imports/no-unused-vars': [
+//         'warn',
+//         { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+//       ]
+//     }
+//   }
+// ];
+
+// export default eslintConfig;
